18d8f09ab344be5c219a5d75952d2ea06ebf6cab
gldeng <realgldeng@gmail.com>
2024-11-18 16:48:42 +0800

    Use aelf namespace for proto files

--- AElf.Launcher/AElf.Firehose/generate.sh
+++ AElf.Launcher/AElf.Firehose/generate.sh
@@ -1,3 +1,3 @@
 #!/bin/bash
 
-protoc --csharp_out=Generated --proto_path=Proto sf/aelf/type/v1/core.proto sf/aelf/type/v1/kernel.proto
+protoc --csharp_out=Generated --proto_path=Proto aelf/core.proto aelf/kernel.proto
