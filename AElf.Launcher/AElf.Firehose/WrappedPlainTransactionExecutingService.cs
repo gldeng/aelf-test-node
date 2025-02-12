8a53db4220ffceb123c0ec874b961c0a71aeacfc
gldeng <realgldeng@gmail.com>
2025-02-12 10:54:52 +0800

    Use reflection to extract original values

--- AElf.Launcher/AElf.Firehose/WrappedPlainTransactionExecutingService.cs
+++ AElf.Launcher/AElf.Firehose/WrappedPlainTransactionExecutingService.cs
@@ -1,7 +1,10 @@
+using System.Reflection;
 using AElf.Kernel;
 using AElf.Kernel.FeatureDisable.Core;
 using AElf.Kernel.SmartContract;
 using AElf.Kernel.SmartContract.Application;
+using AElf.Kernel.SmartContract.Domain;
+using AElf.Types;
 using Microsoft.Extensions.Logging;
 using Microsoft.Extensions.Options;
 using Volo.Abp.EventBus.Local;
@@ -31,29 +34,48 @@ public class WrappedPlainTransactionExecutingService : PlainTransactionExecuting
         SingleTransactionExecutingDto singleTxExecutingDto,
         CancellationToken cancellationToken)
     {
+        var transactionTrace = await base.ExecuteOneAsync(singleTxExecutingDto, cancellationToken);
         if (_options.WithInitialStateTracking)
         {
-            var chainContext = new ChainContextWrapperForTrackingOriginalStateValue(singleTxExecutingDto.ChainContext);
-            singleTxExecutingDto.ChainContext = chainContext;
+            var ctx = singleTxExecutingDto.ChainContext as ChainContextWithTieredStateCache;
+            if (ctx == null)
+            {
+                throw new InvalidOperationException("ChainContext is not of type ChainContextWithTieredStateCache.");
+            }
 
-            var transactionTrace = await base.ExecuteOneAsync(singleTxExecutingDto, cancellationToken);
             await _localEventBus.PublishAsync(new ExtendedTransactionExecutedEventData()
             {
                 TransactionTrace = transactionTrace,
-                OriginalValues = chainContext.OriginalValues
+                OriginalValues = ctx.StateCache.GetOriginalValues()
             });
-            _logger.LogTrace("Executed transaction and published trace with original sate values");
-            return transactionTrace;
+            _logger.LogTrace("Executed transaction and published trace with original state values");
         }
         else
         {
-            var transactionTrace = await base.ExecuteOneAsync(singleTxExecutingDto, cancellationToken);
             await _localEventBus.PublishAsync(new ExtendedTransactionExecutedEventData()
             {
                 TransactionTrace = transactionTrace
             });
             _logger.LogTrace("Executed transaction and published trace");
-            return transactionTrace;
         }
+
+        return transactionTrace;
+    }
+}
+
+public static class TieredStateCacheExtension
+{
+    private static readonly FieldInfo? OriginalValuesField = 
+        typeof(TieredStateCache).GetField("_originalValues", BindingFlags.NonPublic | BindingFlags.Instance);
+
+    public static Dictionary<ScopedStatePath, byte[]> GetOriginalValues(this TieredStateCache cache)
+    {
+        if (OriginalValuesField != null)
+        {
+            return OriginalValuesField.GetValue(cache) as Dictionary<ScopedStatePath, byte[]>
+                   ?? throw new InvalidOperationException("Original values are not of the expected type.");
+        }
+
+        throw new InvalidOperationException("Unable to access the original values.");
     }
 }
